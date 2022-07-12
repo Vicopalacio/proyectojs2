7de93c8024443ad2851a422e5881fd01cf53ba05
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-12 11:47:32 -0300

    realice modificaciones en punto 1

--- js/pto1.js
+++ js/pto1.js
@@ -2,6 +2,6 @@ let edad = parseInt(prompt("Ingrese su edad"))
 
 if(edad >= 18){
     document.write ("Usted puede conducir")
-}if (edad <18){
+} else if (edad <18){
     document.write("Usted no puede conducir")
 }
