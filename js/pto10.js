e91a8b814d805d15e22455c65518ba5fcebffb77
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-13 17:19:18 -0300

    realice la tabla en js

---
+++ js/pto10.js
@@ -0,0 +1,14 @@
+let filas = parseInt(prompt("Ingrese un numero de filas"));
+let columnas = parseInt(prompt("Ingrese un numero de columnas"));
+
+console.log(filas)
+console.log(columnas)
+
+document.write(`<table class="tabla">
+        <tbody>
+            <tr>
+                <td>hola</td>
+                <td>mundo</td>
+            </tr>
+        </tbody>
+    </table>`)
