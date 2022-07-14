253e019e882076dd45f4ecff7d424ac0b3b89789
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-14 17:13:49 -0300

    empece el punto 17 pero no lo termine

--- js/pto17.js
+++ js/pto17.js
@@ -1,3 +1,11 @@
 let texto = prompt("Ingrese un texto");
 console.log(texto.length)
+console.log(texto.charAt())
+console.log(texto)
 
+while(texto >= 0){
+    if (texto.charAt(0) === "a" || texto.charAt(0) === "e" || texto.charAt(0) === "i" || texto.charAt(0) === "o" || texto.charAt(0) === "u"){
+        document.write("la vocal es :" + texto.charAt(0));
+        texto = texto++
+    }
+}
