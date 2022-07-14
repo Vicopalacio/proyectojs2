e66120d2f4a62ccadbb301952acd02f540c4bcd6
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-14 18:26:31 -0300

    avance con punto 17 pero no lo termine

--- js/pto17.js
+++ js/pto17.js
@@ -3,9 +3,15 @@ console.log(texto.length)
 console.log(texto.charAt())
 console.log(texto)
 
-while(texto >= 0){
-    if (texto.charAt(0) === "a" || texto.charAt(0) === "e" || texto.charAt(0) === "i" || texto.charAt(0) === "o" || texto.charAt(0) === "u"){
-        document.write("la vocal es :" + texto.charAt(0));
-        texto = texto++
-    }
+for(let texto; texto.charAt(0) === "a" || texto.charAt(0) === "e" || texto.charAt(0) === "i" || texto.charAt(0) === "o" || texto.charAt(0) === "u"; texto = texto.charAt++){
+    document.write("La vocal es: " + texto.charAt(0))
+    document.write(texto.length)
 }
+
+
+// while(texto >= 0){
+//     if (texto.charAt(0) === "a" || texto.charAt(0) === "e" || texto.charAt(0) === "i" || texto.charAt(0) === "o" || texto.charAt(0) === "u"){
+//         document.write("la vocal es :" + texto.charAt(0));
+//         texto = texto++
+//     }
+// }
