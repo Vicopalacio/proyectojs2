c0d722b143f0ddf2536c22f2dc307e808d09b392
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-18 15:32:24 -0300

    realice punto 14

--- js/pto14.js
+++ js/pto14.js
@@ -1,2 +1,12 @@
 let frase = prompt("Ingrese una frase")
+let salida = ""
 
+for(let i = 0; i < frase.length; i++){
+    let letra = frase[i]
+    console.log(letra)
+    if(letra != " "){
+        letra = letra + "-" 
+    }
+    salida = salida + letra
+    console.log(salida)
+}document.write(salida)
